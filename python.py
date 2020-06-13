abbd35f7ef1bb57958788ed102e3f8664f7fdbdd
Wilsonsem <wilsonsemmwa@gmail.com>
2020-06-13 02:46:33 +0100

    modified python file

--- python.py
+++ python.py
@@ -1,2 +1,10 @@
 print("HELLO WORLD")
-print("sage")
+print("sage") 
+
+#the code below prints "true" if the condition is true and it is an example of indentation
+if (5>1):
+    print("true")
+
+a = b = 4; c=6;
+print (a + b)  #this prints the sum of a and b
+print (c)      #this will print the value of c
