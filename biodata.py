fc4a8aff2c2d6e94faa9c0d86a519eb1f48d43b5
Wilsonsem <wilsonsemmwa@gmail.com>
2020-06-14 00:41:14 +0100

    biodata and python version files added

---
+++ biodata.py
@@ -0,0 +1,12 @@
+#the program below prints my biodata
+
+name= "Semmwa Wilson"
+birthdate= "21st may"
+Address= "Railway Quaters Bauchi."
+skills= "HTML, CSS, Python"
+contact="07057696948"
+email="wlsonsemmwa@gmail.com"
+state="Plateau State"
+nationality="Nigerian"
+craft="sewing"
+print("Hello,\nI am " + name +" a " + nationality +" from "+ state+ "based in " + Address+"\nI am good good with "+skills+" and also a tailor.\nYou can reach via "+contact+ " or "+email)
---
+++ pythonversion.py
@@ -0,0 +1,13 @@
+import sys, platform
+
+print(sys.version)#this printout the dafault version of python installed.
+
+'''
+print(sys.version_info)
+print(sys.version_info[0])
+print(sys.version_info.major)
+print(sys.version_info.minor)
+print(platform.python_version())
+'''
+
+
